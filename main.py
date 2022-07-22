98211399c80ec72a315a5b664c05cc8c6b3eca85
Jorge Villarreal <jorge.vi96@gmail.com>
2022-07-22 01:09:50 -0500

    Changes.

--- main.py
+++ main.py
@@ -4,25 +4,28 @@ import numpy as np
 import pytesseract
 import time
 
-isDead = False
+
+def get_deaths(file_name):
+    try:
+        f = open(file_name, mode='r')
+        deaths = int(f.read().split(": ")[1])
+        f.close()
+        return deaths
+    except FileNotFoundError:
+        f = open(file_name, mode='x')
+        f.write('deaths: 0')
+        return 0
 
 
-def get_deaths():
-    f = open('deaths.txt', mode='r')
-    deaths = int(f.read().split(": ")[1])
+def add_death_to_counter(file_name):
+    deaths = str(get_deaths(file_name) + 1)
+    f = open(file_name, mode='w')
+    f.write("deaths: " + deaths)
     f.close()
-    return deaths
 
 
-def add_death_to_counter():
-    deaths = str(get_deaths() + 1)
-    f = open('deaths.txt', mode='w')
-    f.write("muertes: " + deaths)
-    f.close()
-
-
-def has_died():
-    pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
+def has_died(tesseract_binary_location):
+    pytesseract.pytesseract.tesseract_cmd = tesseract_binary_location
     image = pyautogui.screenshot(region=(780, 500, 1000, 500))
     image = cv.cvtColor(np.array(image), cv.COLOR_RGB2BGR)
 
@@ -53,24 +56,19 @@ def has_died():
 
 
 if __name__ == '__main__':
-
-    # 1) Take a screenshot of the monitor showing the game.
-    # 2) Pass screenshots to Tesseract
-    # 3) Find word "YOU DIED" in the screenshot
-    # 4) If previous step return true, then increase dead counter. (call add_death() method)
-    # 5) Turn dead flag on. (isDead = True)
-    # 6) Repeat steps 1 and 2, until step 3 returns false.
-    # 7) Turn dead flag off. (isDead = False)
-    # 8) End, return to step 1.
+    fileName = 'deaths.txt'
+    tesseractBinaryLocation = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
+    isDead = False
 
     while True:
-        if has_died():
+
+        if has_died(tesseractBinaryLocation):
             isDead = True
-            add_death_to_counter()
-            print(f'Oh no! Deaths: {get_deaths()}')
+            add_death_to_counter(fileName)
+            print(f'Oh no! Deaths: {get_deaths(fileName)}')
 
             while isDead:
-                isDead = has_died()
+                isDead = has_died(tesseractBinaryLocation)
                 time.sleep(1)
 
         time.sleep(1)
